13357fe87eb521b3771066f3c61b843501673570
g.mammanCOS101 <gmghorse@gmail.com>
2024-03-31 22:13:36 +0100

    (third)

---
+++ week_2/practice_3.py
@@ -0,0 +1,14 @@
+items = ['Anaconda', 786, 2.23, 'Jupyter', 70.2]
+shortlist = [321, 'Python']
+
+''' Prints complete list '''
+# Prints first element of the list
+# Prints elements starting from 2nd till 3rd
+# Prints list two times
+print(items + shortlist)  # Prints concatenated Lists
+
+print(items)
+print(items[0])
+print(items[1:3])
+print(items[2:])
+print(shortlist * 2)
