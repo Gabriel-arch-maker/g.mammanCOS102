54b9325e10256b62c970cb3202c373a44623d701
g.mammanCOS101 <gmghorse@gmail.com>
2024-04-07 23:55:32 +0100

    (Fourth)

---
+++ week_3/practice_3.py
@@ -0,0 +1,14 @@
+print("Welcome to the COUPE DE ESVRIVA 2023: FOOTBALL PICKS \n");
+
+captain = {'Madiba: ': 'Chubby Obiora-Okafo', 'Blue-Jays: ': 'Christopher Uaweh' , 'Cirok: ': 'Alexander', 'TSG Walkers: ': 'Ikechukwu'}
+
+goalKeepers = { 'Madiba: ': 'Chubby Obiora-Olafo', 'Blue-Jays: ': 'Pladimaji Abaniwondea/Jeffery Awagu', 'Cirok: ': 'Timileyin Pearse/Izuako Jeremy', 'TSG Walkers: ': 'Ayomide Ojituku'}
+
+for pick in captain:
+    print(pick, captain[pick])
+
+print('\n')
+
+for pick in goalKeepers:
+    print(pick, goalKeepers[pick])
+    
---
+++ week_3/project_1.py
@@ -0,0 +1,6 @@
+girlNames = ['Evelyn', 'Jessica', 'Somto', 'Edith', 'Liza', 'Madonna', 'Waje', 'Tola', 'Aisha', 'Latifa']
+girlAges = [17, 16, 17, 18, 16, 18, 17, 20, 19, 17]
+girlHeights = [5.5, 6.0, 5.4, 5.9, 5.6, 5.5, 6.1, 6.0, 5.7, 5.5]
+girlScores = [80, 85, 70, 60, 76, 66, 87, 95, 50, 49]
+
+boyNames = ['Chinedu', 'Liam', 'Wale', 'Gbenga', 'Abiola', 'Kola', 'Kunle', 'George', 'Thomas', '']
